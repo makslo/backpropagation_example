38767bf22052fdbe361035264bec4e6f3c86c9cf
Maxim Lakin <maximlakin@gmail.com>
2016-10-05 00:54:15 -0700

    refactor

--- feed_forward.py
+++ feed_forward.py
@@ -1,20 +1,21 @@
 import math, random as rd
+import matplotlib.pyplot as plt
 
 class Neuron:
     def __init__(self, size):
         self.weights = [rd.random() for _ in range(size)]
         self.b = rd.random()
 
-    def set_out(inp):
+    def set_out(self,inp):
         s = sum([i*w for i,w in zip(inp,self.weights)])+self.b
         self.inp = inp
         self.out = 1/(1+math.exp(-s))
         return self.out
 
-    def set_diff(d):
+    def set_diff(self,d):
         self.diff = d*self.out*(1-self.out)
 
-    def update(eta):
+    def update(self,eta):
         self.weights = [w - eta*self.diff*i for w,i in zip(self.weights,self.inp)]
 
 # fully connected multilayer feed forward network
@@ -26,13 +27,10 @@ class FeedForwardNN:
         self.target = target
         self.out = []
 
-    def get_n(layer,pos):
-        return layers[layer][pos]
-
-    def add_layer(layer):
+    def add_layer(self,layer):
         self.layers.append(layer)
 
-    def predict():
+    def predict(self):
         inp = self.inputs
         for layer in self.layers:
             out = []
@@ -42,34 +40,52 @@ class FeedForwardNN:
         self.out = inp
         return self.out
 
-    def error():
+    def error(self):
         return 0.5*sum([math.pow((t-o),2) for t,o in zip(self.target, self.out)])
 
-    def update():
-        for layer in layers:
+    def update(self):
+        for layer in self.layers:
             for n in layer:
                 n.update(self.eta)
 
-    def inspect():
+    def inspect(self):
         print(self.inputs)
-        for layer in layers:
+        for layer in self.layers:
             print([n.weights for n in layer])
         print(self.out)
+        print(self.target)
 
-    def train():
+    def train(self):
         # cache the error from previous layer
         d_layer = [[-(t-o) for t,o in zip(self.target, self.out)]] # cache the error from previous layer
-        for layer in layers:
-            for i,n in enumerate(reversed(layer)):
+        for layer in reversed(self.layers):
+            for i,n in enumerate(layer):
                 d = sum([dn[i] for dn in d_layer]) # add up all contributions to n from layer above
                 n.set_diff(d) # update n's gradient
             d_layer = [[n.diff*w for w in n.weights] for n in layer] # backpropagate for next layer
 
 ffnn = FeedForwardNN(target = [0.01, 0.99], inputs = [0.05, 0.1], eta = 0.5)
 
-ffnn.add_layer([Neuron(2),[Neuron(2),[Neuron(2)] # hidden 1
-ffnn.add_layer([Neuron(3),[Neuron(3),[Neuron(3)] # hidden 2
-ffnn.add_layer([Neuron(3),[Neuron(3)]            # output
+ffnn.add_layer([Neuron(2),Neuron(2),Neuron(2)]) # hidden 1
+ffnn.add_layer([Neuron(3),Neuron(3),Neuron(3)]) # hidden 2
+ffnn.add_layer([Neuron(3),Neuron(3)])            # output
+
+ffnn.predict()
+result = {"error": [ffnn.error()], "out0": [ffnn.out[0]], "out1": [ffnn.out[1]]}
+
+for i in range(1000):
+    ffnn.train()
+    ffnn.update()
+    ffnn.predict()
+    result["error"].append(ffnn.error())
+    result["out0"].append(ffnn.out[0])
+    result["out1"].append(ffnn.out[1])
+
+plt.plot(result["out0"])
+plt.plot(result["out1"])
+plt.plot(result["error"])
+plt.show()
+
 
 # print("o1: %s" % out_o1)
 # print("o2: %s" % out_o2)
